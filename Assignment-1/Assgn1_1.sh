dcb1917109431f468c5ec28379d857bb513eebd9
Aman Sharma <sharmaaman2050as@gmail.com>
2023-01-17 15:51:12 +0530

    Reduced wc in 1

--- Assignment-1/Assgn1_1.sh
+++ Assignment-1/Assgn1_1.sh
@@ -1,1 +1,1 @@
-answer=1;for line in $(rev<$1);do x=$answer;y=$line;while [ $y -gt 0 ];do t=$x;x=$y;y=$((t%y));done;answer=$((answer*(line/x)));echo $answer;done|tail -1
+answer=1;for line in $(rev<$1);do((1));x=$answer;y=$line;while true;do((y!=0))||break;t=$x;x=$y;y=$((t%y));done;answer=$((answer*(line/x)));echo $answer;done|tail -1
