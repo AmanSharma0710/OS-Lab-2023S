0c5d63b37971896780409d67ddd3e1757bec7fa7
Aman Sharma <sharmaaman2050as@gmail.com>
2023-01-20 02:42:09 +0530

    Added python script to calculate total wc

---
+++ Assignment-1/checkwc.py
@@ -0,0 +1,11 @@
+groupno = 34
+files = []
+for i in [1, 2, 4, 7, 9]:
+    files.append("Assgn1_{}_{}.sh".format(i, groupno))
+wc = 0
+for file in files:
+    with open(file) as f:
+        content = f.read().replace('=', ' = ').replace('|', ' | ').replace(';', ' ; ').replace(',', ' , ').replace('>', ' > ').replace('<', ' < ')
+        wc += len(content.split())
+        print(file, len(content.split()))
+print("Total: ", wc)
