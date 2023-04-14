6c3dafa4ea9bae108d908fde2bcd525374490a12
ElDestructo3 <indiavishal3@gmail.com>
2023-04-14 14:37:39 +0530

    Added printmutex and debugging printing

--- Assignment-6/goodmalloc.cpp
+++ Assignment-6/goodmalloc.cpp
@@ -15,8 +15,12 @@ int current_scope = 0;
 pthread_mutex_t lockMemory = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lockPageTable = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lockScope = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t printMutex = PTHREAD_MUTEX_INITIALIZER;
 
 int createMemory(int size){
+    pthread_mutex_lock(&printMutex);
+    cout << "createMemory(): Called to create memory of size " << size << "." << endl;
+    pthread_mutex_unlock(&printMutex); 
     pthread_mutex_lock(&lockMemory);
     if(BIG_MEMORY_BLOCK == NULL){
         BIG_MEMORY_BLOCK = new char[size];
@@ -27,9 +31,15 @@ int createMemory(int size){
             node->free = true;
         }
         pthread_mutex_unlock(&lockMemory);
+        pthread_mutex_lock(&printMutex);
+        cout << "createMemory(): Succeeded in creating memory of size " << size << "." << endl;
+        pthread_mutex_unlock(&printMutex); 
         return 0;
     }
     pthread_mutex_unlock(&lockMemory);
+    pthread_mutex_lock(&printMutex);
+    cout << "createMemory(): Failure." << endl;
+    pthread_mutex_unlock(&printMutex); 
     return -1;
 }
 
@@ -69,12 +79,18 @@ void startScope(){
     pthread_mutex_lock(&lockScope);
     current_scope++;
     pthread_mutex_unlock(&lockScope);
+    pthread_mutex_lock(&printMutex);
+    cout << "startScope(): Started scope." << endl;
+    pthread_mutex_unlock(&printMutex); 
 }
 
 void endScope(){
     pthread_mutex_lock(&lockScope);
     current_scope--;
     pthread_mutex_unlock(&lockScope);
+    pthread_mutex_lock(&printMutex);
+    cout << "endScope(): Ended scope."<< endl;
+    pthread_mutex_unlock(&printMutex); 
     //Remove all the entries in the page table that have a scope greater than the current scope
     vector<string> temp;
     pthread_mutex_lock(&lockMemory);
@@ -86,6 +102,9 @@ void endScope(){
         }
     }
     //Mark the nodes in the list as free, and remove the entries from the page table
+    pthread_mutex_lock(&printMutex);
+    cout << "endScope(): Freeing nodes out of scope." << endl;
+    pthread_mutex_unlock(&printMutex);
     for(int i = 0; i < temp.size(); i++){
         Node *node = (Node*)BIG_MEMORY_BLOCK;
         node += pageTable->getOffset(temp[i]);
@@ -95,12 +114,18 @@ void endScope(){
         }
         pageTable->table.erase(temp[i]);
     }
+    pthread_mutex_lock(&printMutex);
+    cout << "endScope() has freed all out-of-scope nodes." << endl;
+    pthread_mutex_unlock(&printMutex);
     pthread_mutex_unlock(&lockPageTable);
     pthread_mutex_unlock(&lockMemory);
 }
 
 List* createList(string name, int len){
     //Check if the list already exists in the page table
+    pthread_mutex_lock(&printMutex);
+    cout << "createList(): Called to create list " << name << " of length " << len << "." << endl;
+    pthread_mutex_unlock(&printMutex);
     pthread_mutex_lock(&lockScope);
     pthread_mutex_lock(&lockMemory);
     pthread_mutex_lock(&lockPageTable);
@@ -142,11 +167,16 @@ List* createList(string name, int len){
             pthread_mutex_unlock(&lockPageTable);
             pthread_mutex_unlock(&lockMemory);
             pthread_mutex_unlock(&lockScope);
+            pthread_mutex_lock(&printMutex);
+            cout << "createList(): Created entry in page table and returning list " << name << " of length " << len << "." << endl;
+            pthread_mutex_unlock(&printMutex);
             return list;
         }
         else{
             //The list cannot fit in memory
+            pthread_mutex_lock(&printMutex);
             cout<<"createList(): The list cannot fit in memory."<<endl;
+            pthread_mutex_unlock(&printMutex);
             pthread_mutex_unlock(&lockPageTable);
             pthread_mutex_unlock(&lockMemory);
             pthread_mutex_unlock(&lockScope);
@@ -157,13 +187,18 @@ List* createList(string name, int len){
     pthread_mutex_unlock(&lockMemory);
     pthread_mutex_unlock(&lockScope);
     //The list already exists in the page table
+    pthread_mutex_lock(&printMutex);
     cout<<"createList(): The list already exists in the page table."<<endl;
+    pthread_mutex_unlock(&printMutex);
     return NULL;
 }
 
 int freeList(string name){
     pthread_mutex_lock(&lockMemory);
     pthread_mutex_lock(&lockPageTable);
+    pthread_mutex_lock(&printMutex);
+    cout << "freeList(): Called to free list " << name << "." << endl;
+    pthread_mutex_unlock(&printMutex);
     //Check if the list exists in the page table
     if(pageTable->findEntry(name)){
         //Mark the nodes in the list as free
@@ -175,13 +210,18 @@ int freeList(string name){
         }
         //Remove the entry from the page table
         pageTable->table.erase(name);
+        pthread_mutex_lock(&printMutex);
+        cout << "freeList(): Freed list entry from page table of name " << name << "." << endl;
+        pthread_mutex_unlock(&printMutex);
         pthread_mutex_unlock(&lockPageTable);
         pthread_mutex_unlock(&lockMemory);
         return 0;
     }
     else{
         //The list does not exist in the page table
+        pthread_mutex_lock(&printMutex);
         cout<<"freeList(): The list does not exist in the page table."<<endl;
+        pthread_mutex_unlock(&printMutex);
         pthread_mutex_unlock(&lockPageTable);
         pthread_mutex_unlock(&lockMemory);
         return -1;
@@ -191,6 +231,9 @@ int freeList(string name){
 int freeMemory(){
     pthread_mutex_lock(&lockMemory);
     pthread_mutex_lock(&lockPageTable);
+    pthread_mutex_lock(&printMutex);
+    cout << "freeMemory(): Called to free all memory. Resetting all page table entries..." << endl;
+    pthread_mutex_unlock(&printMutex);
     if(BIG_MEMORY_BLOCK != NULL){
         //Reset the page table
         while(!pageTable->table.empty()){
@@ -203,10 +246,15 @@ int freeMemory(){
         nodesInMemory = 0;
         pthread_mutex_unlock(&lockPageTable);
         pthread_mutex_unlock(&lockMemory);
+        pthread_mutex_lock(&printMutex);
+        cout << "freeMemory(): Freed all memory." << endl;
+        pthread_mutex_unlock(&printMutex);
         return 0;
     }
     //The memory has already been freed
+    pthread_mutex_lock(&printMutex);
     cout<<"freeMemory(): The memory has already been freed."<<endl;
+    pthread_mutex_unlock(&printMutex);
     pthread_mutex_unlock(&lockPageTable);
     pthread_mutex_unlock(&lockMemory);
     return -1;
@@ -215,7 +263,9 @@ int freeMemory(){
 int assignVal(string name, int index, int val){
     pthread_mutex_lock(&lockMemory);
     pthread_mutex_lock(&lockPageTable);
-
+    pthread_mutex_lock(&printMutex);
+    cout << "assignVal(): Called to assign value " << val << " to list " << name << " at index " << index << "." << endl;
+    pthread_mutex_unlock(&printMutex);
     //Check if the list exists in the page table
     if(pageTable->findEntry(name)){
         //Check if the index is in the list
@@ -230,18 +280,25 @@ int assignVal(string name, int index, int val){
             node->data = val;
             pthread_mutex_unlock(&lockPageTable);
             pthread_mutex_unlock(&lockMemory);
+            pthread_mutex_lock(&printMutex);
+            cout << "assignVal(): Assigned value " << val << " to list " << name << " at index " << index << " in the page table." << endl;
+            pthread_mutex_unlock(&printMutex);
             return 0;
         }
         else{
             //The index is not in the list
+            pthread_mutex_lock(&printMutex);
             cout<<"assignVal(): The index is not in the list."<<endl;
+            pthread_mutex_unlock(&printMutex);
             pthread_mutex_unlock(&lockPageTable);
             pthread_mutex_unlock(&lockMemory);
             return -1;
         }
     }
     //The list does not exist in the page table
+    pthread_mutex_lock(&printMutex);
     cout<<"assignVal(): The list does not exist in the page table."<<endl;
+    pthread_mutex_unlock(&printMutex);
     pthread_mutex_unlock(&lockPageTable);
     pthread_mutex_unlock(&lockMemory);
     return -1;
@@ -250,6 +307,9 @@ int assignVal(string name, int index, int val){
 int getVal(string name, int index){
     pthread_mutex_lock(&lockMemory);
     pthread_mutex_lock(&lockPageTable);
+    pthread_mutex_lock(&printMutex);
+    cout << "getVal(): Called to get value from list " << name << " at index " << index << "." << endl;
+    pthread_mutex_unlock(&printMutex);
     //Check if the list exists in the page table
     if(pageTable->findEntry(name)){
         //Check if the index is in the list
@@ -263,24 +323,34 @@ int getVal(string name, int index){
             //Return the value of the node
             pthread_mutex_unlock(&lockPageTable);
             pthread_mutex_unlock(&lockMemory);
+            pthread_mutex_lock(&printMutex);
+            cout << "getVal(): Returning value " << node->data << " from list " << name << " at index " << index << " in the page table." << endl;
+            pthread_mutex_unlock(&printMutex);
             return node->data;
         }
         else{
             //The index is not in the list
+            pthread_mutex_lock(&printMutex);
             cout<<"getVal(): The index is not in the list."<<endl;
+            pthread_mutex_unlock(&printMutex);
             pthread_mutex_unlock(&lockPageTable);
             pthread_mutex_unlock(&lockMemory);
             return -1;
         }
     }
     //The list does not exist in the page table
+    pthread_mutex_lock(&printMutex);
     cout<<"getVal(): The list does not exist in the page table."<<endl;
+    pthread_mutex_unlock(&printMutex);
     pthread_mutex_unlock(&lockPageTable);
     pthread_mutex_unlock(&lockMemory);
     return -1;
 }
 
 void List::print(){
+    pthread_mutex_lock(&printMutex);
+    cout << "List::print(): Called to print list " << name << "." << endl;
+    pthread_mutex_unlock(&printMutex);
     pthread_mutex_lock(&lockMemory);
     pthread_mutex_lock(&lockPageTable);
     //Check if the list exists in the page table
@@ -298,7 +368,9 @@ void List::print(){
     }
     else{
         //The list does not exist in the page table
+        pthread_mutex_lock(&printMutex);
         cout<<"print(): The list does not exist in the page table."<<endl;
+        pthread_mutex_unlock(&printMutex);
         pthread_mutex_unlock(&lockPageTable);
         pthread_mutex_unlock(&lockMemory);
     }
