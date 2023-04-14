e548cd11fcef8e0da01d0522da23667942db10e3
ElDestructo3 <indiavishal3@gmail.com>
2023-04-14 19:55:21 +0530

    Small changes for convenience

--- Assignment-6/avg_calculator.sh
+++ Assignment-6/avg_calculator.sh
@@ -1,9 +1,11 @@
-for (( i=0; i<100; i++))
+for (( i=0; i<10; i++))
 do
     ./mergesort
 done
-awk '{s+=$1}END{printf("Average time taken:%f\n",s/100.0)}' time.txt
-awk '{s+=$1}END{printf("Average memory used:%f\n",s/100.0)}' mem.txt
+# awk '{s+=$1}END{printf("Average time taken:%f\n",s/10.0)}' time.txt
+# awk '{s+=$1}END{printf("Average memory used:%f\n",s/10.0)}' mem.txt
+# awk '{s+=$1}END{printf("Average number of blocks used:%f\n",s/10.0)}' nodes.txt
 # uncomment below for without freeList
-# awk '{s+=$1}END{printf("Average time taken:%f\n",s/100.0)}' time_without.txt
-# awk '{s+=$1}END{printf("Average memory used:%f\n",s/100.0)}' mem_without.txt
+awk '{s+=$1}END{printf("Average time taken:%f\n",s/20.0)}' time_without.txt
+awk '{s+=$1}END{printf("Average memory used:%f\n",s/20.0)}' mem_without.txt
+awk '{s+=$1}END{printf("Average number of blocks used:%f\n",s/10.0)}' nodes.txt
